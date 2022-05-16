000613606350a5e9a4893ddfc597823c1681ef33
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-16 10:15:36 +0000

    admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'itmes/new'
+    get 'itmes/index'
+    get 'itmes/show'
+    get 'itmes/edit'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
