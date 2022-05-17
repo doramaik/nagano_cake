07439031155eaca8c38a551d405e20d9dd43588a
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-05-17 08:10:08 +0000

    colmn,enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, delivery_preparation: 3, delivered: 4 }
 end
--- db/migrate/20220517030038_create_order_details.rb
+++ db/migrate/20220517030038_create_order_details.rb
@@ -1,7 +1,11 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
-
+      t.integer :order_id
+      t.integer :item_id
+      t.integer :price
+      t.integer :count
+      t.integer :production_status, default: 0
       t.timestamps
     end
   end
