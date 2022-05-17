6ac0351bfd8f6a28a0a44a1385db706c6473afce
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-05-17 09:18:35 +0000

    relations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, delivery_preparation: 3, delivered: 4 }
+
+  has_many :order_details,dependent: :destroy
+  belongs_to :customer
 end
