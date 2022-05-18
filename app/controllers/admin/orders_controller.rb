671037735faaed2178462ef1ee2755f7362b667e
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-05-18 07:27:14 +0000

    [Finish]abmin/order/show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,12 @@ class Admin::OrdersController < ApplicationController
     @customer = @order.customer
     @order = Order.find(params[:id])
     @address = Address.find(params[:id])
+    @order_details = @order.order_details.all
+
+    # 商品合計を出すため
+    @sum = 0
+    @subtotals = @order_details.map { |order_detail| order_detail.once_price * order_detail.quantity }
+    @sum = @subtotals.sum
 
   end
 end
