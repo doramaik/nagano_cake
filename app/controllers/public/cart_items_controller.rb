34a746f03b914c4bab436b66c520be9c2ad18c0d
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-21 05:57:37 +0000

    cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,29 @@
 class Public::CartItemsController < ApplicationController
+
   def index
+    @cart_item = CartItem.all
+
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to request.referer
+  end
+
+  def destroy_all
+
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :count)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,8 @@ class Item < ApplicationRecord
   has_many :cart_items,dependent: :destroy
   belongs_to :genre
 
+  has_one_attached :image
+
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
