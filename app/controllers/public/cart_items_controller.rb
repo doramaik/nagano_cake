a4f083ec26fd06eb75d898dde78b00cc0cd307fe
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-23 06:15:02 +0000

    fix cart_items_controlles

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,7 +11,7 @@ class Public::CartItemsController < ApplicationController
     @cart_item =　current_customer.cart_items.new(cart_item_params)
     # 商品が重複しないようにする　@update_cart_itemは追加しようとしている商品　find_byでカート内に同じモノが存在かを確認
     @update_cart_item = CartItem.find_by(item_id: @cart_item.item_id)
-    # 追加した商品はカート商品と同じ？
+     # 追加した商品はカート商品と同じ？
       if @update_cart_item.present?
        # カート商品は(カート商品+追加商品)　個数のみを足す
        @cart_item.count += @update_cart_item.count
