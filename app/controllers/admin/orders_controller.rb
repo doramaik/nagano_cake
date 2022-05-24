599adf88b527af8420116bf486b9efcbd7182f4c
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-24 07:25:03 +0000

    fix orders_show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,30 +5,15 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-
+    @customer = @order.customer
     @order = Order.find(params[:id])
-    @order_details = @order.order_details
+    @address = Address.find(params[:id])
+    @order_details = @order.order_details.all
 
-
-    # # 商品合計を出すため
+    # 商品合計を出すため
     @sum = 0
     @subtotals = @order_details.map { |order_detail| order_detail.once_price * order_detail.quantity }
     @sum = @subtotals.sum
 
   end
-
-
-  def update
-    # 商品の個数のみを変更する
-    @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to request.referer
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:order_status)
-  end
-
 end
