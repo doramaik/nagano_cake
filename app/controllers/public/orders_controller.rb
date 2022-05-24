8f12978f8d4678d48247dc0aad275ee1d330900a
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-24 08:11:21 +0000

    fix public_order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,13 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
+    # 商品合計を出すため
+    @sum = 0
+    @subtotals = @order_details.map { |order_detail| order_detail.once_price * order_detail.quantity }
+    @sum = @subtotals.sum
+    @order.shipping_fee = 800
+    @order_details = @order.order_details
   end
 
   def new
@@ -17,6 +24,19 @@ class Public::OrdersController < ApplicationController
     @order.customer_id = current_customer.id
     # 3. データをデータベースに保存するためのsaveメソッド実行
     @order.save
+    @order_details = OrderDetail.new
+    @order_details.order_id = @order.id
+    @cart_items = current_customer.cart_items
+
+    @cart_items.each do |cart_item|
+       @order_details.item_id = cart_item.item.id
+       @order_details.price = @order.amount_billed
+       @order_details.amount = cart_item.amount
+
+       @order_details.save
+    end
+    # カート商品を全て削除
+    @cart_items.destroy_all
 
     # 4. リダイレクト
     redirect_to public_orders_thanks_path
@@ -24,7 +44,7 @@ class Public::OrdersController < ApplicationController
 
   def order_confirm
     @order = current_customer.orders.new(order_params)
-    @cart_item = CartItem.all
+    @cart_item = current_customer.cart_items
     @order.shipping_fee = 800
     if params[:order][:address] == "1"
       @order.shipping_address = current_customer.address
