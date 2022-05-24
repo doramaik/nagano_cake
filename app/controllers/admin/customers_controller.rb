9c664a08901686d0e623372c80c50a0cf262c70a
EC2 Default User <ec2-user@ip-172-31-11-12.ap-northeast-1.compute.internal>
2022-05-24 07:55:05 +0000

    2nd

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,8 +15,10 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer)
+      flash[:notice] = '会員情報編集が成功しました'
     else
       render :edit
+      flash[:alret] = '会員情報編集が失敗しました'
     end
   end
 
