8ce1754931e34a053e627d614bc18016e358e25f
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-05-25 07:44:09 +0000

    [Add]genre/index

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @items = Item.all.order("created_at DESC").limit(4)
+    @genres = Genre.all
     # @all_ranks = Item.find(Order.group(:product_id).order('count(product_id) desc').limit(4).pluck(:product_id))
   end
 
