ebe1f1d40321c62db0ca46036588cc45186f976d
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-25 04:25:08 +0000

    fix_order_show

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,10 @@ class OrderDetail < ApplicationRecord
    enum production_status: { production_inpossible: 0, production_pending: 1, in_production: 2, production_complete: 3}
    belongs_to :item
    belongs_to :order
+
+##小計を求めるメソッド
+  def subtotal
+     item.with_tax_price*amount
+  end
+
 end
