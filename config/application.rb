1142153451f65d804bc141e3e97178ec98026c6a
EC2 Default User <ec2-user@ip-172-31-11-12.ap-northeast-1.compute.internal>
2022-05-26 07:07:59 +0000

    develop

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module NaganoCake
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # 日本時間に変更
     config.time_zone = 'Tokyo'
   end
